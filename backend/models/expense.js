fc8b2d20580394d5e921d17c7dea5d593c56dd3b
Roy Gutierrez <roymg29@gmail.com>
2020-07-29 14:38:31 -0400

    added some models for the form and the user

--- backend/models/expense.js
+++ backend/models/expense.js
@@ -2,6 +2,9 @@ const { Schema, model } = require("mongoose");
 
 const expenseSchema = new Schema({
   expenseType: String,
+  frequency: String,
+  date: Date,
+  user: { type: Schema.Types.ObjectId, ref: "User" }, //required: true put later
   //   name: String,
   //   googleId: String,
   //   imageUrl: String,
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,8 +8,18 @@ router.get("/", (req, res, next) => {
 router.post("/expense", async (req, res, next) => {
   console.log(req.body);
   let response = await Expense.create(req.body);
-  console.log(response);
-  res.status(200).json(response);
+  // console.log(response);
+  res.json(response);
 });
-
+router.get("/transactions", async (req, res) => {
+  let response = await Expense.find({ user: "" });
+  // console.log(response);
+  res.json(response);
+});
+router.get("/transactions2", async (req, res) => {
+  console.log(req.query);
+  let response = await Expense.find({ user: req.query.id });
+  // console.log(response);
+  res.json(response);
+});
 module.exports = router;
