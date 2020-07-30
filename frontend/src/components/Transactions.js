6152586a3b40b1bc4d0d18b5d914302459352977
Matthew Manzo <matthew.manzo1@gmail.com>
2020-07-30 12:40:13 -0400

    additions

--- frontend/src/components/Transactions.js
+++ frontend/src/components/Transactions.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import actions from "../services/index";
+import "../components/styles/transactions.css";
+
 class Transactions extends Component {
   state = {
     transactions: [],
@@ -7,7 +9,7 @@ class Transactions extends Component {
   async componentDidMount() {
     console.log("hey", this.props);
     // let res = await actions.transactions();
-    let res = await actions.transactions2("5f206f25da8d992b707a9994");
+    let res = await actions.transactions2("5f20717234bf03224e26785a");
     console.log("whatever", res);
     this.setState({
       transactions: res.data,
@@ -16,9 +18,13 @@ class Transactions extends Component {
   displayTransactions = () => {
     return this.state.transactions.map((eachTransaction) => {
       return (
-        <li>
-          {eachTransaction.expenseType} |{eachTransaction.date} |
-          {eachTransaction.frequency} |{eachTransaction.amount} |
+        <li className="transactions">
+          <div className="transaction-style">
+            <span>{eachTransaction.expenseType}</span>
+            <span>{eachTransaction.frequency}</span>
+            <span>${eachTransaction.amount}</span>
+          </div>
+          {/* |{eachTransaction.date} */}
         </li>
       );
     });
--- frontend/src/components/auth/LogIn.js
+++ frontend/src/components/auth/LogIn.js
@@ -1,31 +1,45 @@
-import React, { Component, Fragment } from 'react';
-import actions from '../../services/index'
+import React, { Component, Fragment } from "react";
+import actions from "../../services/index";
 
 class LogIn extends Component {
+  state = {};
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-    state = {
-
-    } 
-    handleChange = e => this.setState({[e.target.name]: e.target.value})
-
-    handleSubmit = e => {
-        e.preventDefault()
-         actions.logIn(this.state).then(user => {
-            this.props.setUser({...user.data})  
-        }).catch(({ response }) => console.error(response.data));
-    }
-    render() {
-        return (
-            <Fragment>
-                <h2>LogIn</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" onChange={this.handleChange} />
-                    <input name="password" type="password" onChange={this.handleChange} />
-                    <input type="submit" value="Log In"/>
-                </form>
-            </Fragment>
-        );
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    actions
+      .logIn(this.state)
+      .then((user) => {
+        this.props.setUser({ ...user.data });
+      })
+      .catch(({ response }) => console.error(response.data));
+  };
+  render() {
+    return (
+      <Fragment>
+        <h2>Log In</h2>
+        <form onSubmit={this.handleSubmit}>
+          Email:
+          <input
+            placeholder="email"
+            name="email"
+            type="email"
+            onChange={this.handleChange}
+          />
+          <br />
+          Password:
+          <input
+            placeholder="password"
+            name="password"
+            type="password"
+            onChange={this.handleChange}
+          />
+          <br />
+          <input type="submit" value="Log In" />
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default LogIn;
