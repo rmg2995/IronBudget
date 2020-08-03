232f084cbc6d974c70b5321dad096d4e53df6d80
Matthew Manzo <matthew.manzo1@gmail.com>
2020-08-03 15:37:51 -0400

    added months to form filter

--- frontend/src/components/Transactions.js
+++ frontend/src/components/Transactions.js
@@ -149,6 +149,27 @@ class Transactions extends Component {
   render() {
     return (
       <div>
+        <form action="/action_page.php" onSubmit={(e) => this.submitForm()}>
+          <label for="month">Month</label>
+          <select onChange={this.handleChange} name="month" id="">
+            <option value="" disabled selected>
+              Select Month
+            </option>
+            <option value="01">January</option>
+            <option value="02">February</option>
+            <option value="03">March</option>
+            <option value="04">April</option>
+            <option value="05">May</option>
+            <option value="06">June</option>
+            <option value="07">July</option>
+            <option value="08">August</option>
+            <option value="09">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+          <br />
+        </form>
         <button onClick={this.filterTransactions}>Date filter by month</button>
 
         <p>Expense</p>
